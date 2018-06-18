997dde3556c819850859e204c67218a467f4b900
zj <suke.shou@gmail.com>
2018-06-18 17:18:32 +0800

    Edit request to handle error

--- src/http/request.js
+++ src/http/request.js
@@ -1,3 +1,4 @@
+import fetch from 'node-fetch'
 import { getUrl, getParamStr } from './url'
 
 export function get(url, { headers = {}, pathVars = {}, params = {} }) {
@@ -19,10 +20,13 @@ export function remove(url, { headers = {}, pathVars = {} }) {
 async function request(url, method, { headers = {}, pathVars = {}, params = {}, body = {} }) {
   url = getUrl(url, pathVars) + getParamStr(params)
   headers = Object.assign(headers, { 'Content-Type': 'application/json;charset=UTF-8' })
-  const resp = await fetch(url, { method, headers: new Headers(headers), body })
+
+  console.log('API Request =', { url, method, headers, body })
+  const resp = await getResponse(url, method, headers, body)
+  console.log('API Response Status = ', resp.status)
 
   if (!resp.ok) {
-    const error = await resp.json()
+    const error = await getJSON(resp)
     return { body: error, ok: false }
   }
 
@@ -30,6 +34,26 @@ async function request(url, method, { headers = {}, pathVars = {}, params = {}, 
     return { body: null, ok: true }
   }
 
-  const data = await resp.json()
+  const data = await getJSON(resp)
   return { body: data, ok: true }
+}
+
+async function getResponse(url, method, headers, body) {
+  try {
+    return await fetch(url, { method, headers, body: JSON.stringify(body) })
+  } catch (e) {
+    console.error(e.message)
+    return { body: { message: 'error.request.failed' }, ok: false }
+  }
+}
+
+async function getJSON(resp) {
+  try {
+    const json = await resp.json()
+    console.log('API Response Body = ', json)
+    return json
+  } catch (e) {
+    console.error('Error parsing JSON response.', e.message)
+    return null
+  }
 }
